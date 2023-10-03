53eeeb7711e406d288ecb96c69259d25232970eb
오진서 <dhwlstj@gmail.com>
2023-10-03 23:44:31 +0900

    useFetchApi 추가 및 env 설정 추가

---
+++ composables/useFetchApi.ts
@@ -0,0 +1,79 @@
+// import { useOptions } from '@/stores/options';
+// import { useUserInfo } from "@/stores/userInfo";
+interface etc {
+  count: number
+  filename: string
+  useYcount?: number
+  useNcount?: number
+}
+
+export default async <T>(apiUrl: string, opts: {
+  method: any
+  params?: URLSearchParams | object | undefined
+  body?: BodyInit | Record<string, any> | null | undefined
+}, isUri: boolean = true) => {
+  const { VITE_BASE_URL } = import.meta.env;
+//   const useOption = useOptions();
+//   const userInfo = useUserInfo();
+  const etcObj: etc = {
+    count: 0,
+    filename: "",
+    useYcount: 0,
+    useNcount: 0,
+  }
+
+  const fetchData = await useFetch <T> (apiUrl, {
+    baseURL: isUri ? VITE_BASE_URL : '',
+    method: opts.method,
+    params: {...opts?.params },  //uuid: `uuid_${Math.round(Math.random()*1000000)}`
+    // params: opts?.params,
+    body: opts?.body,
+    mode: 'cors',
+    redirect: "follow",
+    cache: "no-store",
+    onRequest({ request, options }) {
+      options.headers = {...options.headers, "Access-Control-Allow-Origin": "*"};
+    },
+    onRequestError({ request, options, error }) {
+    //   useOption.offLoading();
+      // console.log(`[useApiReq] : req 오류 발생`);
+      // console.log(error);
+    },
+    onResponse({ request, response, options }) {
+    //   useOption.offLoading();
+      if(response.headers.get("X-Total-Count")){
+        etcObj.count = Number(response.headers.get("X-Total-Count") || "0")
+      }
+      if(response.headers.get("Content-Disposition")){
+        const str = response.headers.get("Content-Disposition") || ""
+        etcObj.filename = extractQuotes(str) || "";
+      }
+      if(response.headers.get("Use-Y-Count")){
+        etcObj.useYcount = Number(response.headers.get("Use-Y-Count") || "0")
+      }
+      if(response.headers.get("Use-N-Count")){
+        etcObj.useNcount = Number(response.headers.get("Use-N-Count") || "0")
+      }
+      if(response.headers.get("X-Use-Count")){
+        etcObj.useYcount = Number(response.headers.get("X-Use-Count") || "0")
+      }
+      if(response.headers.get("X-Not-Use-Count")){
+        etcObj.useNcount = Number(response.headers.get("X-Not-Use-Count") || "0")
+      }
+      if (response.redirected) {
+        // useOption.setLastPathChg(false)
+        // userInfo.setLogin(false)
+        window.location.href = response.url
+      }
+    },
+    onResponseError({ request, response, options }) {
+    //   useOption.offLoading();
+      // console.log(`[useApiReq] : res 오류 발생`);
+      // console.log(`[useApiReq] : ${response.status}`);
+    },
+  });
+
+
+
+  return {...fetchData, ...etcObj}
+}
---
+++ plugins/plugin.ts
@@ -0,0 +1,11 @@
+export default defineNuxtPlugin((nuxtApp) => {
+
+    // nuxtApp.vueApp.component("PdfApp", PdfApp);
+  
+    return {
+      provide: {
+
+      },
+    };
+  });
+  
